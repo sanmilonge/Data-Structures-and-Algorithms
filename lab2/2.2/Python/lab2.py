a5db980882342a70c76b5aae8a35dc1aec8eaf8d
sanmilonge <139220972+sanmilonge@users.noreply.github.com>
2026-03-28 01:38:50 +0000

    Fixed folders

--- lab2/2.2/Python/lab2.py
+++
@@ -1,8 +0,0 @@
-year_1grades = [97, 93, 90]
-print(year_1grades)
-year_2grades = year_1grades.copy()
-year_2grades.extend([100, 100, 100])
-print(year_2grades)
-import sys
-print(f"\nSize of year_1grades: {sys.getsizeof(year_1grades)} bytes")
-print(f"Size of year_2grades: {sys.getsizeof(year_2grades)} bytes")
